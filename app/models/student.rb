c3f9f5e347304e28271999a4a62bf73d934a6087
Andy Agus <Redagus@gmail.com>
2020-06-28 12:56:26 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,13 @@
 class Student < ApplicationRecord
+	has_many :ducks
+
+
+	def list_ducks
+		self.ducks.map do |duck| 
+				duck.name
+		end
+	end
+
+	validates :name, presence: true
+	validates :mod, numericality: {greater_than: 0, less_than: 6}
 end
