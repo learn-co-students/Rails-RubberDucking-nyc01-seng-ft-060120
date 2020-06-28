4d190ab689eabb809acc695e6913f3e2f1b735ea
scubaguba345 <brandon.gubitosa@gmail.com>
2020-06-28 19:16:29 -0400

    need to figure out how to put associations to student here

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,50 @@
 class StudentsController < ApplicationController
 
+  before_action :student_method, only: [:show, :edit, :update]
+
   def index
     @students = Student.all
+  end
+
+  def show
+  
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.create(student_params)
+    if @student.valid?
+      redirect_to student_path(@student)
+    else           
+      flash[:my_errors] = @student.erros.full_messages
+      redirect_to new_student_path
+    end
+  end
+
+  def edit
+  
+  end
+
+  def update
+    if @student.update(student_params)
+    redirect_to student_path(@student)
+    else   
+      flash[:my_errors] = @student.erros.full_messages
+      redirect_to edit_student_path
+    end
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name, :mod)
+  end
+
+  def student_method
+    @student = Student.find(params[:id])
   end
 
 end
--- app/models/duck.rb
+++ app/models/duck.rb
@@ -1,2 +1,11 @@
 class Duck < ApplicationRecord
+    belongs_to :student
+    
+
+    validates :name, presence: true
+	validates :description, presence: true
+    validates :student_id, presence: true
+
+    
+    
 end
