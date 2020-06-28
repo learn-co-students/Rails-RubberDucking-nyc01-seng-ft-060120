e45b81221cc662967f83e7f83ddedf8c491f2242
mayalexandera <maya.blevins@gmail.com>
2020-06-27 23:17:29 -0400

    Done.

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -1,7 +1,36 @@
 class DucksController < ApplicationController
-
+  before_action :set_duck, only: [:edit, :show, :update]
   def index
     @ducks = Duck.all
+  end
+
+  def new
+    @duck = Duck.new
+  end
+  
+  def create
+    @duck = Duck.create(duck_params)
+    redirect_to @duck
+  end
+
+  def edit
+  end
+
+  def update
+    @duck.update(duck_params)
+    redirect_to @duck
+  end
+
+  def show
+  end
+
+  private
+  def duck_params
+    params.require(:duck).permit(:name, :description, :student_id, student_attributes: [:name, :id])
+  end
+
+  def set_duck
+    @duck = Duck.find(params[:id])
   end
 
 end
