d995e1e1f8aa1cf95228b72cf321c07bf4493ca8
shadman-a <shadman@outlook.com>
2020-06-28 23:09:14 -0400

    Done.

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -4,4 +4,36 @@ class DucksController < ApplicationController
     @ducks = Duck.all
   end
 
+  def new
+    @duck = Duck.new
+  end
+
+  def show
+    @duck = Duck.find(params[:id])
+  end
+
+  def create
+    @duck = Duck.create(params[:id])
+
+    redirect_to duck_path(@duck.id)
+  end
+
+  def update
+    @duck = Duck.find(params[:id])
+    @duck.update(duck_params)
+
+    redirect_to duck_path(@duck.id)
+  end
+
+  def edit
+    @duck = Duck.find(params[:id])
+  end
+
+  private
+
+  def duck_params
+    params.require(:duck).permit(:name, :description)
+  end
+  
+   
 end
--- db/migrate/20200628204354_create_ducks.rb
+++ db/migrate/20200628204354_create_ducks.rb
@@ -1,8 +1,10 @@
 class CreateDucks < ActiveRecord::Migration[5.2]
   def change
     create_table :ducks do |t|
+      t.belongs_to :student, foreign_key: true
       t.string :name
       t.string :description
+
 
       t.timestamps
     end
