56edcfdc387c9b541fa55567f0c91bb2a81ef70f
berellevy <berellevy@gmail.com>
2020-06-29 21:34:13 -0400

    Done.

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -1,7 +1,52 @@
 class DucksController < ApplicationController
-
+before_action :set_duck, only: [:show, :edit, :update, :destory]
+  
   def index
     @ducks = Duck.all
   end
 
+  def show
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    if @duck.update_attributes(strong_params)
+      flash[:success] = "Duck was successfully updated"
+      redirect_to @duck
+    else
+      flash[:errors] = @duck.errors.full_messages
+      redirect_to edit_duck_path(@duck)
+    end
+  end
+
+  def new
+    @duck = Duck.new
+  end
+  
+  
+  def create
+    @duck = Duck.new(strong_params)
+    if @duck.save
+      flash[:success] = "Duck successfully created"
+      redirect_to @duck
+    else
+      flash[:errors] = @duck.errors.full_messages
+      redirect_to new_duck_path
+    end
+  end
+  
+
+  private
+
+  def set_duck
+    @duck = Duck.find(params[:id])
+  end
+  
+  def strong_params
+    params.require(:duck).permit(:name, :description, :student_name)
+  end
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,4 +4,48 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def show
+    @student = Student.find(params[:id])
+    @ducks = @student.ducks
+  end
+  
+  def new
+    @student = Student.new
+    @student.ducks.build
+  end
+  
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      flash[:success] = "Student successfully created"
+      redirect_to @student
+    else
+      flash[:errors] = @student.errors.full_messages
+      redirect_to new_student_path 
+    end
+  end
+  
+  
+  def edit
+    @student = Student.find(params[:id])
+  end
+  
+  def update
+    @student = Student.find(params[:id])
+      if @student.update_attributes(student_params)
+        flash[:success] = "Student was successfully updated"
+        redirect_to @student
+      else
+        flash[:errors] = @student.errors.full_messages
+        redirect_to edit_student_path(@student)
+      end
+  end
+  
+  private
+
+  def student_params
+    params.require(:student).permit(:name, :mod)
+  end
+  
+
 end
